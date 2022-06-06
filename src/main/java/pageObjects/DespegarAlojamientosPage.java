61b3f1a993ac62910b55983938ff4256b9ead401
Seba <sebakonicoff@hotmail.com>
2022-06-06 09:40:08 -0300

    detalles

--- src/main/java/pageObjects/DespegarAlojamientosPage.java
+++ src/main/java/pageObjects/DespegarAlojamientosPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
 public class DespegarAlojamientosPage {
 	private WebDriver driver = null;
